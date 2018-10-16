c3fc2d73250df49e5be1b8169599b26542f5bd48
zis <desert73eagle@gmail.com>
2018-10-16 11:21:29 +0300

    Initial Commit

---
+++ System View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  System View Controllers
+//
+//  Created by student on 16.10.2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
